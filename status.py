72c022866089c5199b9adcb33dd3f9b05a8469f9
ethanboxx <ethanboxx@gmail.com>
2017-08-29 10:40:48 +0100

    Pin number varibles setup

--- status.py
+++ status.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python2
 # Python script for controling status light and shutdown button on raspberry pi
 # Author : Ethan Brierley
+
+ledPin = 1 # Pin for simple status LED
+powerButtonPin = 2 # Pin for power switch for restart
+statusButtonPin = 3 # Pin for showing the status of the server on the LED
