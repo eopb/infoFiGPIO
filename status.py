e5d3e5137edf715a46762638c78315211f9897cd
ethanboxx <ethanboxx@gmail.com>
2017-08-29 11:05:43 +0100

    Reboot LED setup.

--- status.py
+++ status.py
@@ -2,6 +2,31 @@
 # Python script for controling status light and shutdown button on raspberry pi
 # Author : Ethan Brierley
 
-ledPin = 1 # Pin for simple status LED
-powerButtonPin = 2 # Pin for power switch for restart
-statusButtonPin = 3 # Pin for showing the status of the server on the LED
+from gpiozero import LED
+from gpiozero import Button
+from time import sleep
+import sys
+import subprocess
+
+ledPin = LED(1) # Pin for simple status LED
+powerButtonPin = Button(2) # Pin for power switch for restart
+statusButtonPin = Button(3) # Pin for showing the status of the server on the LED
+
+def Poweroff():
+    wiringpi.softPwmWrite(pinOn,0)
+    Clean()
+    time.sleep(1)
+    command = "/usr/bin/sudo /sbin/reboot now"
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    print (str(output))
+    print ("end")
+
+while True:
+    if powerButtonPin.is_pressed:
+        ledPin.on()
+        time.sleep(0.5)
+        ledPin.off()
+        Poweroff()
+        
+    
