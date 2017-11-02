ef64f200ac5a20005ca89f912f9962d3d45dde3d
example <example@example.com>
2017-11-02 16:58:14 +0000

    More logs

--- autolaunchlog.sh
+++ autolaunchlog.sh
@@ -1,6 +1,9 @@
 
 #!/bin/sh
-sudo rm -f /home/pi/logs/cronlog2
+sudo rm -f /home/pi/logs/cronlog5
+sudo mv /home/pi/logs/cronlog4  /home/pi/logs/cronlog5
+sudo mv /home/pi/logs/cronlog3  /home/pi/logs/cronlog4
+sudo mv /home/pi/logs/cronlog2  /home/pi/logs/cronlog3
 sudo mv /home/pi/logs/cronlog  /home/pi/logs/cronlog2
 sudo rm -f /home/pi/logs/cronlog
 
