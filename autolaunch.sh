16e02d0e3d39852952237eeed9d54112d7d45fe1
example <example@example.com>
2017-11-02 17:03:54 +0000

    Better comments

--- autolaunch.sh
+++ autolaunch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
+# This script is for 
+# Navigate to home directory, then to the home directory, then make a logs folder, then execute python script, then back home.
 
 cd /
 cd home/pi
