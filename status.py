f32bdc2282ccc411c10d2d36c9f7e10bf17242c9
Ethan Brierley <ethanboxx@gmail.com>
2019-04-11 11:01:25 +0100

    format

--- status.py
+++ status.py
@@ -1,19 +1,21 @@
+import subprocess
+import sys
+import time
+from gpiozero import Button
+from gpiozero import LED
++
 #!/usr/bin/env python2
 # Python script for controling status light and shutdown button on raspberry pi
 # Author : Ethan Brierley
-#services are dnsmasq, hostapd, apache2
+# services are dnsmasq, hostapd, apache2
 
-print ("Importing libraries")
+print("Importing libraries")
 
-from gpiozero import LED
-from gpiozero import Button
-import time
-import sys
-import subprocess
 
-ledPin = LED(14) # Pin for simple status LED
-powerButtonPin = Button(18) # No longer needed.
-statusButtonPin = Button(22) # Pin for showing the status of the server on the LED
+ledPin = LED(14)  # Pin for simple status LED
+powerButtonPin = Button(18)  # No longer needed.
+# Pin for showing the status of the server on the LED
+statusButtonPin = Button(22)
 
 greenPin = LED(4)
 bluePin = LED(27)
@@ -21,38 +23,42 @@ redPin = LED(17)
 
 start_time = time.time()
 
+
 def Poweroff():
     ledPin.on()
     time.sleep(1)
-    print ("Shutting down")
+    print("Shutting down")
     command = "/usr/bin/sudo /sbin/reboot now"
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
-    print (str(output))
-    print ("end")
+    print(str(output))
+    print("end")
+
 
 def clean():
-    print ("Cleaning Pins")
+    print("Cleaning Pins")
     ledPin.off()
     redPin.off()
     greenPin.off()
+
+
 def checkForKeyWords(service):
-    print (service)
+    print(service)
     if "dnsmasq" not in service:
-        print ("dnsmasq not found")
+        print("dnsmasq not found")
         return False
     if "hostapd" not in service:
-        print ("hostapd not found")
+        print("hostapd not found")
         return False
     if "apache2" not in service:
-        print ("apache2 not found")
+        print("apache2 not found")
         if "systemd+apache22apache226apache2" not in service:
-            print ("systemd+apache22apache226apache2 not found")
+            print("systemd+apache22apache226apache2 not found")
             return False
         return True
 
-
     return True
+
 
 def checkStatus():
     proc = subprocess.Popen('pstree', stdout=subprocess.PIPE)
@@ -74,15 +80,11 @@ def checkStatus():
         return 2
     return 1
 
+
 def moveLogs():
-    print ("Reorganizing logs and removing old logs. ")
+    print("Reorganizing logs and removing old logs. ")
     pass
-    #Code that moves logs.
-
-
-
-
-
+    # Code that moves logs.
 
 
 clean()
@@ -96,7 +98,7 @@ while True:
         pass
     if checkStatus() == 2:
         redPin.on()
-        print ("debug stuff")
+        print("debug stuff")
     else:
         redPin.off()
         if numberOfRuns > 2:
@@ -104,9 +106,9 @@ while True:
             time.sleep(1)
             greenPin.off()
             numberOfRuns = 0
-    if time.time() - start_time > 86400: #86400 is the number of seconds in 24hours.
-        print ("Script has been running for 24 hours")
+    # 86400 is the number of seconds in 24hours.
+    if time.time() - start_time > 86400:
+        print("Script has been running for 24 hours")
         start_time = time.time()
         moveLogs()
     time.sleep(15)
-
